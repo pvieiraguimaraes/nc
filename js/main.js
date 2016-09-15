9a9e5201f47307c12f2d587698af1595c9bee066
Pedro Henrique <pvieiraguimaraes@gmail.com>
2016-09-15 13:30:01 -0300

    Adicionando favicon e selo cartoleiro pro

--- js/main.js
+++ js/main.js
@@ -106,7 +106,7 @@ var Narnia = {};
         var parcial_rodada = total_pontos.toFixed(2);
         var slug_time = data.time.slug;
         var pontos_ordenacao = (total_pontos == 0.00 && !mercadoFechado) ? pontos : total_pontos;
-        var pro = data.time.assinante == true ? '<img src="https://cartolafc.globo.com/dist/0.6.9/img/selo-cartoleiro-pro.svg" class="cartola-pro">' : '';
+        var pro = data.time.assinante == true ? '<img src="img/selo-cartoleiro-pro.svg" class="cartola-pro">' : '';
 
         $('#narnia-table').append('<tr class="' + slug_time + '" data-row="' + slug_time + '" data-total="' + pontos_ordenacao + '"><td colspan="1"><div class="col-xs-12">' + pro + '<img src="' + imgEscudo + '" style="width: 50px;"><img style="width: 30px;position: absolute;left: 45px;top: 25px;" src="' + imgPerfil + '" class="img-circle"></div></td><td colspan="3"><h3>' + nome + '</h3><p>' + nome_cartola + '</p></td><td colspan="2"><p class="ponto" style="text-align: center">' + pontos + '</p></td><td colspan="2" style="text-align: center"><p class="pontoparcial">' + parcial_rodada + '</p></td><td colspan="2" style="text-align: center;"><p class="patrimonio">' + patrimonio + '</p></td><td colspan="2" style="text-align: center" class="coca"></td></tr>');
         if (atletas_html != '')
